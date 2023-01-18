815574dcb0501f5eebee270ac823b25c2506bf88
Sebastien Gault <sgault.webdev@gmail.com>
2023-01-18 16:08:16 +0100

    fixed bug when upload file and cancel new bill after

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -17,11 +17,11 @@ export default class NewBill {
   }
   handleChangeFile = e => {
     e.preventDefault()
-    const file = this.file.files[0]
+    this.SelectedFile = this.file.files[0]
     const filePath = e.target.value.split(/\\/g)
-    const fileName = filePath[filePath.length-1]
-    if(file) {
-      const fileType = file.type
+    this.fileName = filePath[filePath.length-1]
+    if(this.SelectedFile) {
+      const fileType = this.SelectedFile.type
       if(fileType !== 'image/png' && fileType !== 'image/jpeg') {
         console.log('bad file type')
         this.addError(this.file, 'Mauvais format de fichier, Merci de fournir un fichier jpg, jpeg ou png')
@@ -32,32 +32,18 @@ export default class NewBill {
     }
     this.removeError(this.file)
     this.file.classList.remove('error')
-    const formData = new FormData()
+    this.formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
-
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+    this.formData.append('file', this.SelectedFile)
+    this.formData.append('email', email)
+    this.formData.append('fileName', this.fileName)
   }
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
     const email = JSON.parse(localStorage.getItem("user")).email
     const bill = {
-      email,
+      email: email,
       type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
       name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
       amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
@@ -65,20 +51,34 @@ export default class NewBill {
       vat: e.target.querySelector(`input[data-testid="vat"]`).value,
       pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
       commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
-      fileUrl: this.fileUrl,
+      fileUrl: undefined,
       fileName: this.fileName,
       status: 'pending'
     }
-    this.updateBill(bill)
+    this.formData.set('email', bill.email)
+    this.formData.set('type', bill.type)
+    this.formData.set('name', bill.name)
+    this.formData.set('amount', bill.amount)
+    this.formData.set('date', bill.date)
+    this.formData.set('vat', bill.vat)
+    this.formData.set('pct', bill.pct)
+    this.formData.set('commentary', bill.commentary)
+    this.formData.set('fileUrl', undefined)
+    this.formData.set('status', bill.status)
+
+    this.createBill(this.formData)
     this.onNavigate(ROUTES_PATH['Bills'])
   }
 
   // not need to cover this function by tests
-  updateBill = (bill) => {
+  createBill = (bill) => {
     if (this.store) {
       this.store
       .bills()
-      .update({data: JSON.stringify(bill), selector: this.billId})
+      .create({data: bill,
+                headers: {
+          noContentType: true
+        }})
       .then(() => {
         this.onNavigate(ROUTES_PATH['Bills'])
       })
@@ -92,7 +92,10 @@ export default class NewBill {
     el.parentElement.appendChild(errorSpan)
   }
   removeError(el) {
-    const parent = el.parentElement
-    parent.querySelector('.error-msg').remove()
+    const error = el.parentElement.querySelector('.error-msg')
+    if(error){
+      error.remove()
+    }
+    
   }
 }
